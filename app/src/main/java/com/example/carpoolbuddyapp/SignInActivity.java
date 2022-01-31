310f7c862f9e9fdbe78f923d75cd34be005d76e3
Daniel Yang <imdanielyang@gmail.com>
2022-01-31 22:33:08 +0800

    sign up works

--- app/src/main/java/com/example/carpoolbuddyapp/SignInActivity.java
+++ app/src/main/java/com/example/carpoolbuddyapp/SignInActivity.java
@@ -2,20 +2,77 @@ package com.example.carpoolbuddyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SignInActivity extends AppCompatActivity
 {
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
+
+    private String selected;
+    private Spinner sUserType;
+
+    private EditText emailField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        emailField = findViewById(R.id.signInEmailInput);
+        passwordField = findViewById(R.id.signInPasswordInput);
+    }
+
+    public void signIn(View v)
+    {
+        System.out.println("Sign inned");
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+
+        System.out.println("EMAIL HERE:" + emailString);
+        System.out.println("EMAIL HERE:" + passwordString);
+
+//        FirebaseUser mUser = mAuth.getCurrentUser();
+//
+//        firestore.collection("users").document();
+//
+//        firestore.collection("    ").document().addSnapshotListener(new OnCompleteListener<DocumentSnapshot>())
+//        {
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task)
+//            {
+//                if(task.isSuccessful())
+//                {
+//
+//                }
+//                else
+//                {
+//
+//                }
+//            }
+//        }
+    }
+
+    public void updateUI(FirebaseUser currentUser)
+    {
+        if (currentUser != null)
+        {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/carpoolbuddyapp/SignUpActivity.java
+++ app/src/main/java/com/example/carpoolbuddyapp/SignUpActivity.java
@@ -1,21 +1,48 @@
 package com.example.carpoolbuddyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
+
+    private String selected;
+    private Spinner sUserType;
+
+    private EditText nameField;
+    private EditText emailField;
+    private EditText passwordField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        nameField = findViewById(R.id.signUpNameInput);
+        emailField = findViewById(R.id.signUpEmailInput);
+        passwordField = findViewById(R.id.signUpPasswordInput);
 
         Spinner spinner = findViewById(R.id.signUpRoleInput);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.roles, android.R.layout.simple_spinner_item);
@@ -35,5 +62,40 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
     public void onNothingSelected(AdapterView<?> adapterView)
     {
 
+    }
+
+    public void signUp(View v)
+    {
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Log.d("Sign Up", "Signed Up!");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        }
+                        else
+                        {
+                            Log.w("SIGN UP", "createUserWithEmail:failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void updateUI(FirebaseUser currentUser)
+    {
+        if (currentUser != null)
+        {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
