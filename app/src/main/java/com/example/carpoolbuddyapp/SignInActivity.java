6e129c95bbda8cd599a58f073da6c4f4c8e6838e
Daniel Yang <imdanielyang@gmail.com>
2022-01-31 19:52:34 +0800

    supposed to work

--- app/src/main/java/com/example/carpoolbuddyapp/SignInActivity.java
+++ app/src/main/java/com/example/carpoolbuddyapp/SignInActivity.java
@@ -3,8 +3,13 @@ package com.example.carpoolbuddyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SignInActivity extends AppCompatActivity
+public class SignInActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
 
     @Override
@@ -12,5 +17,23 @@ public class SignInActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Spinner spinner = findViewById(R.id.signUpRoleInput);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.roles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+    {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
     }
 }
