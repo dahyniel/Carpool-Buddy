b788c5cb37565d65d05d2f888c113ce72c6e7fcb
Daniel Yang <imdanielyang@gmail.com>
2022-02-06 13:59:36 +0800

    recycler view sort of working

--- app/src/main/java/com/example/carpoolbuddyapp/UserProfileActivity.java
+++ app/src/main/java/com/example/carpoolbuddyapp/UserProfileActivity.java
@@ -1,17 +1,23 @@
 package com.example.carpoolbuddyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class UserProfileActivity extends AppCompatActivity
+import java.util.ArrayList;
+
+public class UserProfileActivity extends AppCompatActivity implements MyVehiclesRecyclerViewAdapter.ItemClickListener
 {
+    MyVehiclesRecyclerViewAdapter adapter;
     Button addVehicleButton;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
@@ -22,18 +28,29 @@ public class UserProfileActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        addVehicleButton = findViewById(R.id.addVehicleButton);
-        addVehicleButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                openAddVehicleActivity();
-            }
-        });
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.myVehiclesRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyVehiclesRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
-    public void openAddVehicleActivity()
+    @Override
+    public void onItemClick(View view, int position)
+    {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    public void openAddVehicleActivity(View v)
     {
         Intent intent = new Intent(this, AddVehicleActivity.class);
         startActivity(intent);
